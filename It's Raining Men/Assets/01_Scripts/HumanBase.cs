eccc96b0f827da13cc5f22a0bf5807b6a7f3f335
DuHerbst <du.herbst@gmail.com>
2026-05-09 09:02:02 -0600

    First falling human

--- It's Raining Men/Assets/01_Scripts/HumanBase.cs
+++ It's Raining Men/Assets/01_Scripts/HumanBase.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class HumanBase : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float gravityScale;
+    [SerializeField] private int pointValue;
+    //private bool _hasBounced; -- to be implemented if we have time!
+    private Rigidbody2D _rigidbody;
+    
     void Start()
     {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody.gravityScale = gravityScale;
         
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    // void WasBounced()
+    // {
+    //     _hasBounced = true;
+    // }
+    
+    void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.CompareTag("ScoreZone"))
+        {
+            //GameManager.Instance.AddScore(pointValue);
+            Destroy(gameObject);
+        }
+
+        if (other.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+            //Game Over, go to end game scene
+        }
         
     }
+    
 }
