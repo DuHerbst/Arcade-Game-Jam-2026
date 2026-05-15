4639e049059f35bdd92f7a7c545eda18f00d87dd
Snoopdawg21 <65247774+Snoopdawg21@users.noreply.github.com>
2026-05-15 15:29:29 -0600

    final timing adjustments
    changed the time on the blinking and shit, so it looks less awful, and less- strobe light lmao

--- Assets/Scripts/WarningFlash.cs
+++ Assets/Scripts/WarningFlash.cs
@@ -7,17 +7,21 @@ public class WarningFlash : MonoBehaviour
     [SerializeField] private Sprite yellow;
     [SerializeField] private Sprite red;
     [SerializeField] private float waitTimer;
+    private bool timerBool = true;
     private bool spriteFlipper;
     
     void Update()
     {
-        StartCoroutine(SpriteChangeTimer());
+        if(timerBool)
+            StartCoroutine(SpriteChangeTimer());
     }
 
     private IEnumerator SpriteChangeTimer()
     {
+        timerBool = false;
         warningSprite.sprite = spriteFlipper ?  yellow : red;
         yield return new WaitForSeconds(waitTimer);
         spriteFlipper = !spriteFlipper;
+        timerBool = true;
     }
 }
